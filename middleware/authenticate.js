cbe5067807aa59605e530f712692d45ab2caf88a
SushankSinha <SushankSinha>
2023-08-16 12:43:56 +0530

    Server Error resolved

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,9 +3,9 @@ import User from '../models/userSchema.js'
 
 const authenticate = async(req, res, next) => {
     try{
-        const token = req.cookies.token;
+        const token = req.header('token');
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-        const rootUser = await User.findOne({_id:verifyToken._id, "tokens.token" : token});
+        const rootUser = await User.findOne({_id:verifyToken._id, 'tokens.token' : token});
         if(!rootUser){
             throw new Error('User not found')
         }
