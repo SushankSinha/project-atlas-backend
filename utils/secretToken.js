85a34b51c099a837e48c92da75845f0272eb5c6d
SushankSinha <SushankSinha>
2023-08-23 00:31:28 +0530

    authenticate

--- utils/secretToken.js
+++ utils/secretToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 dotenv.config({path : './.env'});
 
 const secretToken = (id) => {
-  return jwt.sign({ id }, process.env.TOKEN_KEY, {
+  return jwt.sign({ id }, process.env.SECRET_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
 };
