3f55c4022bb27cc5b8e689cb0dcc5329cc109910
SushankSinha <SushankSinha>
2023-10-21 01:38:26 +0530

    http update

--- router/authentication.js
+++ router/authentication.js
@@ -3,6 +3,9 @@ const router = express.Router();
 import bcrypt from 'bcryptjs';
 import User from '../models/userSchema.js';
 import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+router.use(cookieParser())
 
 router.post('/register', async (req, res) => {
 
