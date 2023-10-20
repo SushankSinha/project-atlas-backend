70faf7bc25380fa69125ccd766941be13aaf7041
SushankSinha <SushankSinha>
2023-10-21 02:00:41 +0530

    cookie Update

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(
 
 app.use(express.json());
 // app.use(cookieParser());
-app.use(auth);
 app.use(express.urlencoded({extended: true}));
+app.use(auth);
 app.use(calendarController)
 app.use(task)
 
